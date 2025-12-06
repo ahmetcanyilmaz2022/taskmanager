41907359008a96bc720f09b467e0ec414622ef57
ahmet can yılmaz <ahmetcan@192.168.1.9>
2025-12-06 23:48:51 +0300

    re

--- app/main.py
+++ app/main.py
@@ -7,29 +7,32 @@ app = FastAPI()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+
 def get_db():
-conn = psycopg2.connect(DATABASE_URL)
-return conn
+    conn = psycopg2.connect(DATABASE_URL)
+    return conn
+
 
 class Task(BaseModel):
-title: str
-description: str
+    title: str
+    description: str
+
 
 @app.get("/")
 def root():
-return {"message": "Task Manager API running!"}
+    return {"message": "Task Manager API running!"}
+
 
 @app.post("/tasks")
 def create_task(task: Task):
-conn = get_db()
-cur = conn.cursor()
-cur.execute(
-"INSERT INTO tasks (title, description) VALUES (%s, %s) RETURNING id",
-(task.title, task.description),
-)
-new_id = cur.fetchone()[0]
-conn.commit()
-cur.close()
-conn.close()
-return {"id": new_id, "task": task}
-
+    conn = get_db()
+    cur = conn.cursor()
+    cur.execute(
+        "INSERT INTO tasks (title, description) VALUES (%s, %s) RETURNING id",
+        (task.title, task.description),
+    )
+    new_id = cur.fetchone()[0]
+    conn.commit()
+    cur.close()
+    conn.close()
+    return {"id": new_id, "task": task}
